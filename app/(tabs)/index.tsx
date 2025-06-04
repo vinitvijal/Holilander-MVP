36e3611712560f3b9dd974ffc47500fb7da58884
Vinu <vineet.22566@sscbs.du.ac.in>
2025-06-05 01:56:45 +0530

    basic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,13 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-
-
+import { Text, View } from 'react-native';
 import '../global.css';
+
 
 export default function HomeScreen() {
   return (
-      <SafeAreaView>
-        <View>
-          <Text className=' text-white'>Hello</Text>
+        <View className=' pt-12 bg-white h-screen-safe-or-0'>
+          <View>
+            <Text className='text-3xl font-bold'>Welcome Back</Text>
+          </View>
         </View>
-      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 import './global.css';
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,12 +15,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="light" />
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
