b12b28547db0c6f5d39b7a870c676ff6d8a1ddc4
Vinu <vineet.22566@sscbs.du.ac.in>
2025-06-05 15:47:02 +0530

    layout correction

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -4,38 +4,46 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-        <Tabs
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarShowLabel: true,
-        tabBarIcon: ({ color, size }) => {
-          const icons: Record<string, React.ComponentProps<typeof Ionicons>['name']> = {
-            home: 'home-outline',
-            task1: 'list-outline',
-            task2: 'checkmark-circle-outline',
-            task3: 'clipboard-outline',
-            profile: 'person-outline',
-          };
-          return <Ionicons name={icons[route.name] as React.ComponentProps<typeof Ionicons>['name']} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#007aff',
-        tabBarInactiveTintColor: '#888',
-        tabBarStyle: {
-          height: 70,
-          paddingBottom: 10,
-          paddingTop: 10,
-        },
-      })}
+<View className=' h-screen-safe-or-0 bg-white'>
+    <Tabs
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarShowLabel: true,
+      tabBarIcon: ({ color, size }) => {
+        const icons: Record<string, React.ComponentProps<typeof Ionicons>['name']> = {
+          index: 'home-outline',
+          book: 'list-outline',
+          timesheet: 'checkmark-circle-outline',
+          find: 'search-outline',
+          profile: 'person-outline',
+        };
+        return <Ionicons name={icons[route.name] as React.ComponentProps<typeof Ionicons>['name']} size={size} color={color} className=' ' />;
+      },
+      tabBarActiveTintColor: '#007aff',
+      tabBarInactiveTintColor: '#888',
+      tabBarStyle: {
+        height: 70,
+        paddingBottom: 10,
+        paddingTop: 10,
+        borderRadius: 20,
+      },
+    })}
     >
-      <Tabs.Screen name="home" options={{ title: 'Home' }} />
-      <Tabs.Screen name="task1" options={{ title: 'Task 1' }} />
-      <Tabs.Screen name="task2" options={{ title: 'Task 2' }} />
-      <Tabs.Screen name="task3" options={{ title: 'Task 3' }} />
+      <Tabs.Screen name="find" options={{ title: 'Find' }} />
+      <Tabs.Screen name="book" options={{ title: 'Book' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="timesheet" options={{ title: 'Timesheet' }} />
       <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
     </Tabs>
+          <StatusBar style="auto" />
+
+  </View>
   );
 }
---
+++ app/(home)/book.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import '../global.css';
+
+
+export default function HomeScreen() {
+  return (
+        <View className=' pt-12 bg- h-screen-safe-or-0'>
+          <View>
+            <Text className='text-3xl font-bold'>Welcome Back</Text>
+          </View>
+        </View>
+  );
+}
