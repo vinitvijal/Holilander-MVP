51604d25e9f96b1093ffe0aa7e13caeaad1302cb
Vinu <vineet.22566@sscbs.du.ac.in>
2025-06-07 00:59:20 +0530

    attendance log

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,20 +1,25 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
-import { Text, TouchableHighlight, View } from "react-native";
+import { SafeAreaView, ScrollView, Text, TouchableHighlight, TouchableOpacity, View } from "react-native";
 import "../global.css";
 
 export default function HomeScreen() {
   return (
-    <View className="">
+    <SafeAreaView className=" h-screen-safe-or-0 bg-zinc-100">
+      <View className=" absolute top-0 w-full bg-transparent blur-md h-20 "/>
+
+    <ScrollView overScrollMode="never" >
       <Hero />
       <MainCard />
-    </View>
+      <AttendanceLog />
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
 function Hero() {
   return (
-    <View id="Hero" className=" pt-12 bg-first h-60">
+    <View id="Hero" className=" z-0  top-0 bg-first h-52">
       <View className=" pt-6 relative flex justify-between items-center flex-row px-4">
         <View className="flex flex-row items-center gap-2">
           <Ionicons
@@ -64,7 +69,7 @@ function MainCard() {
 
 
   return (
-    <View className="bg-white -top-16 rounded-2xl flex gap-4 items-center  shadow-sm py-6 px-5 m-4">
+    <View className="bg-white -top-16 rounded-2xl flex gap-4 items-center  py-6 px-5 mx-4 mt-4">
       <View className=" w-[100%] p-2 rounded-full bg-first flex items-center justify-center">
         <Text className="text-lg font-semibold text-white">
           Upcoming Schedule
@@ -75,7 +80,7 @@ function MainCard() {
       </View>
         <View className=" w-full flex items-center justify-between flex-row">
           <Text className="text-   font-light">Monday, 8th June</Text>
-          <Text className="font-semibold">Tallaght hospital</Text>
+          <Text className="font-semibold">Tallaght Hospital</Text>
         </View>
 
       <View className=" w-full flex items-center justify-between pt-4 flex-row">
@@ -109,4 +114,84 @@ function MainCard() {
         </View>
     </View>
   );
+}
+
+
+
+function AttendanceLog(){
+  const attendanceData = [
+    {
+      date: "02 June 2025",
+      day: "Thursday",
+      checkIn: "08:00",
+      checkOut: "17:00",
+      CheckInTime: true,
+      CheckOutTime: true,
+    },
+    {
+      date: "01 June 2025",
+      day: "Wednesday",
+      checkIn: "08:15",
+      checkOut: "17:05",
+      CheckInTime: true,
+      CheckOutTime: true,
+    },
+    {
+      date: "31 May 2025",
+      day: "Tuesday",
+      checkIn: "08:10",
+      checkOut: "17:00",
+      CheckInTime: true,
+      CheckOutTime: false,
+    },
+    {
+      date: "30 May 2025",
+      day: "Monday",
+      checkIn: "08:05",
+      checkOut: "17:00",
+      CheckInTime: false,
+      CheckOutTime: true,
+    },
+    {
+      date: "29 May 2025",
+      day: "Sunday",
+      checkIn: "08:20",
+      checkOut: "17:10",
+      CheckInTime: true,
+      CheckOutTime: true,
+    },
+   
+  ]
+  return (
+    <View className=" -top-6 px-6 ">
+        <View className="flex flex-row justify-between items-center">
+          <Text className="text-2xl font-bold">Attendance Log</Text>
+          <TouchableOpacity>
+            <Text className="text-md text-second">View All</Text>
+          </TouchableOpacity>
+        </View>
+
+          {attendanceData.map((data, index) => <View key={index} className=" border-b py-4 px-2 border-zinc-300 my-4 flex flex-row justify-between" >
+            <View className=" flex-1">
+              <Text className=" font-semibold text-lg">{data.day}</Text>
+              <Text className="text-zinc-500 ">{data.date}</Text>
+            </View>
+
+            <View className=" flex flex-row gap-4">
+            <View className=" flex items-center">
+              <Text className={" font-bold text-lg "  + (data.CheckInTime ? 'text-emerald-600' : 'text-red-600')}>{data.checkIn}</Text>
+              <Text className="text-zinc-500">Check In</Text>
+            </View>
+
+            <View className=" flex items-center">
+              <Text className={" font-bold text-lg " + (data.CheckOutTime ? 'text-emerald-600' : 'text-red-600')}>{data.checkOut}</Text>
+              <Text className="text-zinc-500">Check Out</Text>
+            </View>
+            </View>
+
+        </View> )}
+
+       
+    </View>
+  )
 }
